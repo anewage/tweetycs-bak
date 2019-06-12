b83d3ad913cd80b337528728227af874a6cc2f45
Amir Haghighati <haghighati.amir@gmail.com>
2019-06-11 20:53:17 -0400

    add get method

--- controllers/api/tweetController.js
+++ controllers/api/tweetController.js
@@ -9,13 +9,23 @@ export default class TweetsController extends Controller {
      */
     init () {
         this.post('/tweet/save', this.saveTweet)
+        this.get('/tweets', this.getTweets)
     }
 
-    async saveTweet (request, h) {
+    async saveTweet(request, h) {
         try {
-            var tweet = new Tweet(request.payload.tweet)
+            let tweet = new Tweet(request.payload.tweet);
             await tweet.save()
             return 'OK'
+        } catch (e) {
+            Boom.badRequest()
+        }
+    }
+
+    getTweets(request, h) {
+        try {
+            let sankeyData = Tweet.aggregate({$group: {_id: '$cause_factors', }})
+                .select('_id id id_str text source user retweet_count favorite_count entities created_at')
         } catch (e) {
             Boom.badRequest()
         }
