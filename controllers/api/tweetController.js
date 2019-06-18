62b75bc35f2bed03e47e53565a80390a9ac2e78b
Amir Haghighati <haghighati.amir@gmail.com>
2019-06-17 21:29:38 -0400

    update get method

--- controllers/api/tweetController.js
+++ controllers/api/tweetController.js
@@ -9,23 +9,106 @@ export default class TweetsController extends Controller {
      */
     init () {
         this.post('/tweet/save', this.saveTweet)
-        this.get('/tweets', this.getTweets)
+        this.get('/aggregate', this.getTweets)
     }
 
     async saveTweet(request, h) {
         try {
             let tweet = new Tweet(request.payload.tweet);
             await tweet.save()
+            console.log('DONE')
             return 'OK'
         } catch (e) {
             Boom.badRequest()
         }
     }
 
-    getTweets(request, h) {
+    async getTweets(request, h) {
         try {
-            let sankeyData = Tweet.aggregate({$group: {_id: '$cause_factors', }})
-                .select('_id id id_str text source user retweet_count favorite_count entities created_at')
+            let group_topics = await Tweet.aggregate([
+                {
+                    '$project': {
+                        'labels': 1,
+                        'keywords': 1,
+                        'topics': {
+                            '$objectToArray': '$topics'
+                        }
+                    }
+                }, {
+                    '$unwind': {
+                        'path': '$topics',
+                        'includeArrayIndex': 'topic_index'
+                    }
+                }, {
+                    '$unwind': {
+                        'path': '$labels',
+                        'includeArrayIndex': 'unwinded_label_index',
+                        'preserveNullAndEmptyArrays': true
+                    }
+                }, {
+                    '$group': {
+                        '_id': {
+                            'bucket': '$labels.id',
+                            'group': '$labels.result.group',
+                            'topic': '$topics.k'
+                        },
+                        'count': {
+                            '$sum': 1
+                        }
+                    }
+                }, {
+                    '$group': {
+                        '_id': '$_id.bucket',
+                        'details': {
+                            '$push': '$$ROOT'
+                        }
+                    }
+                }
+            ])
+            let theme_topics = await Tweet.aggregate([
+                {
+                    '$project': {
+                        'labels': 1,
+                        'keywords': 1,
+                        'topics': {
+                            '$objectToArray': '$topics'
+                        }
+                    }
+                }, {
+                    '$unwind': {
+                        'path': '$topics',
+                        'includeArrayIndex': 'topic_index'
+                    }
+                }, {
+                    '$unwind': {
+                        'path': '$labels',
+                        'includeArrayIndex': 'unwinded_label_index',
+                        'preserveNullAndEmptyArrays': true
+                    }
+                }, {
+                    '$group': {
+                        '_id': {
+                            'bucket': '$labels.id',
+                            'theme': '$labels.result.theme',
+                            'topic': '$topics.k'
+                        },
+                        'count': {
+                            '$sum': 1
+                        }
+                    }
+                }, {
+                    '$group': {
+                        '_id': '$_id.bucket',
+                        'details': {
+                            '$push': '$$ROOT'
+                        }
+                    }
+                }
+            ])
+            return {
+                group_topics: group_topics,
+                theme_topics: theme_topics
+            }
         } catch (e) {
             Boom.badRequest()
         }
