da739b08f0a1bd4fd521af80b12c483f85706527
Amir Haghighati <haghighati.amir@gmail.com>
2019-11-25 18:24:08 -0500

    add custom labeling

--- controllers/api/summaryController.js
+++ controllers/api/summaryController.js
@@ -1,6 +1,6 @@
 const { Controller } = require('bak')
 const Boom = require('boom')
-const { Summary } = require('../../models')
+const { Summary, Tweet } = require('../../models')
 
 export default class TweetsController extends Controller {
 
@@ -24,20 +24,35 @@ export default class TweetsController extends Controller {
         try {
             // noinspection JSDeprecatedSymbols
             console.log('got a summary', request.payload.custom_theme, request.payload.custom_group)
-            let summary = new Summary(request.payload);
 
+            // Save the summary
+            let summary = new Summary(request.payload);
             const summs = await Summary.find()
             let found = false
             for (const summ of summs) {
                 // Check for themes
-                if(summary.equals(summ))
+                if (summary.equals(summ))
                     found = true
                 // Check for groups
             }
             if (!found)
                 await summary.save()
+
+            // Update the tweet
+            let tweet = await Tweet.findOne({id_str: request.payload.tweet.id_str})
+            tweet.labels = summary.labels
+            tweet.labels.push({
+                id: 'custom',
+                title: 'User Customized Labeling',
+                result: {
+                    theme: summary.custom_theme,
+                    group: summary.custom_group
+                }
+            })
+            tweet.save()
             return 'OK'
-        } catch (e) {
+        }
+         catch (e) {
             Boom.badRequest()
         }
     }
